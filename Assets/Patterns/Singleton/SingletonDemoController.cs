6cc9b1ea529c571e0917a05d93febe9411bfe71f
Mattias Westman <mattiaswestman@outlook.com>
2024-12-04 20:44:50 +0100

    Reorganized assets. Added WIP scripts for more patterns.

--- Assets/Patterns/Singleton/SingletonDemoController.cs
+++ Assets/Patterns/Singleton/SingletonDemoController.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 namespace Personal.Patterns
 {
-    public class DemoController : MonoBehaviour
+    public class SingletonDemoController : MonoBehaviour
     {
         private void Update()
         {
